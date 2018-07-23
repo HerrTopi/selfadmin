1efb84cbfb0cf1ea800a8142247afa3d23b5c6d2
HerrTopi <toppantoster@gmail.com>
2018-07-23 22:56:47 +0200

    hulladek ie update

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,13 +1,13 @@
 import { EXAMPLE_ACTION } from "./actionList";
 import { USER_DETAILS } from "./actionList";
 
-export const changeStoreData = data => {
+export const changeStoreData = (data) => {
   return {
     type: EXAMPLE_ACTION,
     data
   };
 };
-export const userDetails = vipcode => {
+export const userDetails = (vipcode) => {
   return {
     type: USER_DETAILS,
     vipcode
--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Example from "../containers/ExampleContainer";
 import Details from "../containers/DetailsContainer";
-import Admin from "./Admin";
+//import Admin from "./Admin";
 import Email from "./Email";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 
@@ -15,9 +15,9 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Example} />
-          <Route exact path="/admin" component={Admin} />
-          <Route exact path="/details" component={Details} />
           <Route exact path="/emailsender" component={Email} />
+
+          <Route exact path="/details" component={Details} />
         </div>
       </Router>
     );
@@ -25,4 +25,5 @@ class App extends Component {
 }
 export default App;
 
-//<Route exact path="/emailsender" component={Email} />
+
+//<Route exact path="/admin" component={Admin} />
